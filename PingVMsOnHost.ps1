43818df92d5d21c79e88b0d7ea3d497eef4ee2ef
Mann5112 <cmann@vectren.com>
2014-10-21 16:56:19 -0500

    Update PingVMsOnHost.ps1
    Updated to ping hostname from vmtools instead of VM name. Can't stand having a poorly written script in public!

--- PingVMsOnHost.ps1
+++ PingVMsOnHost.ps1
@@ -1,14 +1,9 @@
 <#
-
 .LABEL
-
 Ping-VMs-On-Host
 
 .DESCRIPTION
-
-Performs ping of all VMs on an ESX host. Successful pings are displayed in white and failed pings are displayed in red.
-
-
+Performs continuous of all VMs on an ESX host. Successful pings are displayed in green and failed pings are displayed in red.
 #>
 
 param
@@ -17,18 +12,19 @@ param
    [VMware.VimAutomation.ViCore.Types.V1.Inventory.VMHost]
    $Phost
 );
-$computers = Get-VMHost | Where-Object {$_.Name -like "$Phost*"} | GET-VM | Select-Object -Property Name
+
+$computers = Get-VMHost | Where-Object {$_.Name -like "$Phost*"} | GET-VM
 do {
 $i=1
 Foreach ($computer in $computers) {
 $i++
-$computer = ($computer).name
+$computer = ($computer).guest.HostName
 $results = gwmi -query "SELECT * FROM Win32_PingStatus WHERE Address = '$computer'"
 if ($results.StatusCode -eq 0) {
-Write-Host "$computer is Pingable"
+Write-Host "$computer hostname is pingable"
 }
 else {
-Write-Error "$computer is not Pingable"
+Write-Error "$computer hostname is not pingable"
 }
 }
 }
